4c31c68977e85b43604f7d8094474a6a5716421f
agleichs <aglei002@plattsburgh.edu>
2025-09-25 12:04:10 +0000

    fiddling

--- code/Site50_stream_lake_champlain_data.R
+++ code/Site50_stream_lake_champlain_data.R
@@ -11,7 +11,7 @@ library(dataRetrieval)
 
 
 # If working in cyverse, set working directory and project root
-setwd("/home/agleichs/data-store/stressors-food-web-connectivity-stability-innovation-summit-2025_4")
+setwd("/home/joyan/data-store/stressors-food-web-connectivity-stability-innovation-summit-2025_4")
 #here::i_am("README.md")   # or any file guaranteed to exist in the project
 
 # Site code for "50 - Missisquoi bay"
